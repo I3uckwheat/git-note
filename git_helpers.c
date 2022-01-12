ec9b45105b7e53a2825a9d7bc16ca0630a230c50
Briggs Elsperger <briggse@protonmail.com>
2022-01-11 18:49:29 -0700

    Add git_helper code

---
+++ git_helpers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+#include <./git_helpers.h>
+
+int get_branch_name(char *buffer, size_t bufferSize) {
+    FILE *fp;
+    fp = popen("git branch --show-current", "r");
+    if(fp == NULL) {
+        printf("Failed to access git");
+	return 1;
+    }
+
+    while(fgets(buffer, bufferSize, fp) != NULL);
+    buffer[strcspn(buffer, "\n")] = 0; // Removes the newline returned from the git command
+
+    pclose(fp);
+    return 0;
+}
+
+int get_dir_name(char *buffer, size_t bufferSize) {
+	return 1;
+}
---
+++ git_helpers.h
@@ -0,0 +1,4 @@
+#pragma once
+
+int get_branch_name(char *buffer, size_t bufferSize);
+int get_dir_name(char *buffer, size_t bufferSize);
