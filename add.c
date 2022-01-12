1427bbd1100b0ec8099fd83345f8d3bd25eb22c5
Briggs Elsperger <belsperger@goguardian.com>
2022-01-12 08:12:20 -0700

    Use git config to get default editor and open

--- add.c
+++ add.c
@@ -5,15 +5,24 @@
 #include <unistd.h>
 
 #include <./file_helpers.h>
+#include <./git_helpers.h>
+
+int open_editor() {
+    char note_path[4096];
+    get_note_path(note_path, sizeof(note_path));
+    
+    char configured_editor_path[4096];
+    get_configured_editor_path(configured_editor_path, sizeof(configured_editor_path));
+
+    char *arg_list[] = { "vim", note_path, NULL };
+    execv(configured_editor_path, arg_list);
+    return 0;
+}
 
 int add(int argc, char *argv[]) {
     char *message = argv[2];
     if(message == NULL) {
-        char note_path[4096];
-        get_note_path(note_path, sizeof(note_path));
-        char *arg_list[] = { "vim", note_path, NULL };
-        // TODO: make more robust
-        execv("/bin/vim", arg_list);
+        return open_editor();
     }
 
     FILE* noteFile = open_notes_file("a");
--- git_helpers.h
+++ git_helpers.h
@@ -2,3 +2,4 @@
 
 int get_branch_name(char *buffer, size_t bufferSize);
 int get_dir_name(char *buffer, size_t bufferSize);
+int get_configured_editor_path(char *buffer, size_t bufferSize);
