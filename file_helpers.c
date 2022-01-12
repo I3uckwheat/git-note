d30f8143d15ea8689e89e24d777b7b3ed3cfe9c8
Briggs Elsperger <briggse@protonmail.com>
2022-01-11 19:22:47 -0700

    Format file_helpers

--- file_helpers.c
+++ file_helpers.c
@@ -7,35 +7,35 @@
 #include <./file_helpers.h>
 #include <./git_helpers.h>
 
-FILE *open_notes_file(char* mode) {
+FILE *open_notes_file(char *mode) {
     // Git branch lengths are limited to 255 characters
     char branchName[256];
     get_branch_name(branchName, 256);
 
-	// 256 reserved for git branch and \0
-	char noteDirPath[4096 - 256];
-	// TODO: Add project name
-	snprintf(noteDirPath, sizeof(noteDirPath), "%s/%s", getenv("HOME"), ".notes");
+    // 256 reserved for git branch and \0
+    char noteDirPath[4096 - 256];
+    // TODO: Add project name
+    snprintf(noteDirPath, sizeof(noteDirPath), "%s/%s", getenv("HOME"), ".notes");
 
-	char notePath[4096];
-	snprintf(notePath, sizeof(notePath), "%s/%s", noteDirPath, branchName);
+    char notePath[4096];
+    snprintf(notePath, sizeof(notePath), "%s/%s", noteDirPath, branchName);
 
-	FILE* note = fopen(notePath, mode);
-	// Failing to open the file, is likely due to a missing directory
-	if(note == NULL && errno == ENOENT) {
-		if(mkdir(noteDirPath, 0775) != 0) {
-			printf("Failed to create note path\n");
-			exit(1);
-		}
+    FILE *note = fopen(notePath, mode);
+    // Failing to open the file, is likely due to a missing directory
+    if (note == NULL && errno == ENOENT) {
+        if (mkdir(noteDirPath, 0775) != 0) {
+            printf("Failed to create note path\n");
+            exit(1);
+        }
 
-		// Recurse to attempt to open file again
-		return open_notes_file(mode);
-	} else if(note == NULL) {
+        // Recurse to attempt to open file again
+        return open_notes_file(mode);
+    } else if (note == NULL) {
         printf("%s: Failed to open notes file\n", strerror(errno));
     }
 
-	return note;
-};
+    return note;
+}
 
 void close_notes_file(FILE *noteFile) {
 	fclose(noteFile);
