6d8e366faae52641d01bf8790636864bd460775d
Briggs Elsperger <briggse@protonmail.com>
2025-03-13 22:05:35 -0600

    Makes install script more robust if existing file

--- install.sh
+++ install.sh
@@ -9,6 +9,8 @@ then
     cmake ..
     make
     mkdir --parents $INSTALL_DIR
+    rm $INSTALL_DIR/git-note
     mv ./git-note $INSTALL_DIR/git-note
+    echo "------------------------------------"
     echo "Add '\$HOME/.bin/' to your bashrc/zshrc/fshrc/etc file to use git-note"
 fi
